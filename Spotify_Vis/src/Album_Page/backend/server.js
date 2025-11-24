1a4411de4a740215ab021cd3d115ecdb72062f7d
danieljmoon <danieljmoon@Daniels-MacBook-Pro.local>
2025-11-24 12:16:39 -0500

    backend

--- Spotify_Vis/src/Album_Page/backend/server.js
+++ Spotify_Vis/src/Album_Page/backend/server.js
@@ -8,6 +8,8 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(cors());
+app.use(express.static(path.join(__dirname, "public")));
+
 
 // ===== Load Spotify Listening History =====
 
